0cf43bc2ce395a618b79fc8059ec4c481db347fa
guptahani75@gmail.com <guptahani75@gmail.com>
2026-07-25 17:20:38 +0530

    feat: fix image folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,11 @@
+import Hero from "../src/components/sections/Hero"
+
+
 export default function Home() {
   return (
     <main>
-      <section
-        id="home"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">Home Section</h1>
-      </section>
-
-      <section
-        id="stats"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">Stats</h1>
-      </section>
-
-      <section
-        id="clients"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">Clients</h1>
-      </section>
-
-      <section
-        id="edge"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">Accredian Edge</h1>
-      </section>
-
-      <section
-        id="cat"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">CAT</h1>
-      </section>
-
-      <section
-        id="how-it-works"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">How It Works</h1>
-      </section>
-
-      <section
-        id="faq"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">FAQ</h1>
-      </section>
-
-      <section
-        id="testimonials"
-        className="flex h-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold">Testimonials</h1>
-      </section>
+      <Hero />
+      
     </main>
   );
 }
