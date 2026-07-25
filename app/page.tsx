f20a9c3191f6a41ceafab543192c955169173c95
guptahani75@gmail.com <guptahani75@gmail.com>
2026-07-25 17:50:59 +0530

    feat: render stats into page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "../src/components/sections/Hero"
-
+import Hero from "@/src/components/sections/Hero";
+import Stats from "@/src/components/sections/Stats";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      
+      <Stats />
     </main>
   );
 }
