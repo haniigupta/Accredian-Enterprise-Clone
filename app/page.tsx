20fd938d7b62aaa5b669508b0bbe252dde85226a
guptahani75@gmail.com <guptahani75@gmail.com>
2026-07-25 17:52:16 +0530

    feat: render client into page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/src/components/sections/Hero";
 import Stats from "@/src/components/sections/Stats";
+import Clients from "@/src/components/sections/Clients";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Stats />
+      <Clients />
     </main>
   );
 }
