1b747c8db1543e43e0353c9a3d0f9e67f7ef9513
guptahani75@gmail.com <guptahani75@gmail.com>
2026-07-26 14:50:40 +0530

    feat: create course segmentation data constant

---
+++ src/constants/courseSegmentation.ts
@@ -0,0 +1,26 @@
+export const courseSegmentation = [
+  {
+    title: "Program Specific",
+    image: "/courses/program-specific.webp",
+    subtitle:
+      "Certificate, Executive, Post Graduate Certificate",
+  },
+  {
+    title: "Industry Specific",
+    image: "/courses/industry-specific.webp",
+    subtitle:
+      "IT, Healthcare, Retail, Finance, Education, Manufacturing",
+  },
+  {
+    title: "Topic Specific",
+    image: "/courses/topic-specific.webp",
+    subtitle:
+      "Machine Learning, Design, Analytics, Cybersecurity, Cloud",
+  },
+  {
+    title: "Level Specific",
+    image: "/courses/level-specific.webp",
+    subtitle:
+      "Senior Leadership, Mid-Career Professionals, Freshers",
+  },
+];
